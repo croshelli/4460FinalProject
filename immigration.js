702dfd75c686abe43e9f9832ba181cbc07366412
croshelli <roshellikatie33@gmail.com>
2014-02-09 16:19:45 -0500

    data/ edit d3

---
+++ immigration.js
@@ -0,0 +1,11 @@
+
+
+//create constant buffers for margin, as well as variables for height, width
+// bottom edge and right edge of frame
+var margin = {top:40, right:20, bottom: 30, left:40};
+var width = 1000-margin.left-margin.right;
+var height = 500-margin.top-margin.bottom;
+var frameBase = 500-margin.bottom;
+var frameRight = 1000-margin.right;
+
+
