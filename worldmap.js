c2881d52ebe2d9de40a7d6b998a7887db9f95b3a
whughes8 <wesjhughes@gatech.edu>
2014-03-20 11:06:22 -0400

    Latest versions of my map

--- worldmap.js
+++ worldmap.js
@@ -2,14 +2,23 @@ d3.csv("testCSV.csv", function(error, data){
 
 createMap(data);
 
-}); 
+});
+var projectionMain = d3.geo.equirectangular()
+	.center([0, -100])
+	.rotate([0, 0])
+	.scale(150)
+	.translate([document.getElementById('container').offsetWidth / 2, document.getElementById('container').offsetWidth / 2]);
+var pathMain = d3.geo.path()
+	.projection(projectionMain);
+
 var bombMap;
 var europeColor = '#9467bd';
-var africaColor = '';
-var southAmericaColor = '';
-var northAmericaColor = '';
+var africaColor = '#ffa500';
+var southAmericaColor = '#008000';
+var northAmericaColor = '#800080';
 var asiaColor = '#ff0000';
-var austrailaColor = '';
+var oceaniaColor = '#0000ff';
+var centralAmericaColor = '#ffff00';
 
 function createMap(data){
 	data.forEach(function(d){
@@ -18,11 +27,30 @@ function createMap(data){
 	redrawMap(0,0,0,0,150);
 	bubbles();
 	console.log(bombMap);
-	bombMap.svg.selectAll('.datamaps-bubble').on('click', function() {
-		redrawMap(0,0,0,0,400);
-	});
+	
+	bombMap.svg.selectAll('.datamaps-bubble')
+		.on('click', function() {
+			//redrawMap(0,0,0,0,400);
+				
+				projectionMain.transition()
+				.center([this.cx, this.cy])
+				.rotate([0, 0])
+				.scale(400)
+				//need to find a way to update the scale correctly
+				.translate([document.getElementById('container').offsetWidth / 2,  document.getElementById('container').offsetHeight / 2]);
+				pathMain.transition()
+				.projection(projectionMain)
+				.duration(100);
+			
+			}
+			);
+			
+		
+	};	
+		
+	
 
-};	
+	
 
 	
 	function redrawMap(center1,center2,rotate1,rotate2,scale){
@@ -37,14 +65,7 @@ function createMap(data){
 			},
 			
 			setProjection: function(element) {
-				var projection = d3.geo.equirectangular()
-				.center([center1, center2])
-				.rotate([rotate1, rotate2])
-				.scale(scale)
-				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-				var path = d3.geo.path()
-				.projection(projection);
-				return {path: path, projection: projection}; 
+				return {path: pathMain, projection: projectionMain}; 
 			},
 		
 			fills: {
@@ -84,10 +105,39 @@ function createMap(data){
 				'TWN': asiaColor,
 				'TUR': asiaColor,
 				'VNM': asiaColor,
-				
-				'CDN': northAmericaColor,
+				'EGY': africaColor,
+				'ETH': africaColor,
+				'LBR': africaColor,
+				'MAR': africaColor,
+				'ZAF': africaColor,
+				'AUS': oceaniaColor,
+				'NZL': oceaniaColor,
+				'CAN': northAmericaColor,
+				'USA': northAmericaColor,
 				'MEX': northAmericaColor,
-				'CUBA': northAmericaColor,
+				'CUB': northAmericaColor,
+				'DOM': northAmericaColor,
+				'HTI': northAmericaColor,
+				'JAM': northAmericaColor,
+				'BLZ': centralAmericaColor,
+				'CRI': centralAmericaColor,
+				'SLV': centralAmericaColor,
+				'GCA': centralAmericaColor,
+				'HND': centralAmericaColor,
+				'NIC': centralAmericaColor,
+				'PAN': centralAmericaColor,
+				'ARG': southAmericaColor,
+				'BOL': southAmericaColor,
+				'BRA': southAmericaColor,
+				'CHL': southAmericaColor,
+				'COL': southAmericaColor,
+				'ECU': southAmericaColor,
+				'GUY': southAmericaColor,
+				'PRY': southAmericaColor,
+				'PER': southAmericaColor,
+				'SUR': southAmericaColor,
+				'URY': southAmericaColor,
+				'VEN': southAmericaColor,
 				
 				
 				defaultFill: '#000000'
@@ -129,6 +179,40 @@ function createMap(data){
 				'TWN': {fillKey: 'TWN'},
 				'TUR': {fillKey: 'TUR'},
 				'VNM': {fillKey: 'VNM'},
+				'EGY': {fillKey: 'EGY'},
+				'ETH': {fillKey: 'ETH'},
+				'LBR': {fillKey: 'LBR'},
+				'MAR': {fillKey: 'MAR'},
+				'ZAF': {fillKey: 'ZAF'},
+				'AUS': {fillKey: 'AUS'},
+				'NZL': {fillKey: 'NZL'},
+				'USA': {fillKey: 'USA'},
+				'CAN': {fillKey: 'CAN'},
+				'MEX': {fillKey: 'MEX'},
+				'CUB': {fillKey: 'CUB'},
+				'DOM': {fillKey: 'DOM'},
+				'HTI': {fillKey: 'HTI'},
+				'JAM': {fillKey: 'JAM'},
+				'BLZ': {fillKey: 'BLZ'},
+				'CRI': {fillKey: 'CRI'},
+				'SLV': {fillKey: 'SLV'},
+				'GCA': {fillKey: 'GCA'},
+				'HND': {fillKey: 'HND'},
+				'NIC': {fillKey: 'NIC'},
+				'PAN': {fillKey: 'PAN'},
+				'ARG': {fillKey: 'ARG'},
+				'BOL': {fillKey: 'BOL'},
+				'BRA': {fillKey: 'BRA'},
+				'CHL': {fillKey: 'CHL'},
+				'COL': {fillKey: 'COL'},
+				'ECU': {fillKey: 'ECU'},
+				'GUY': {fillKey: 'GUY'},
+				'PRY': {fillKey: 'PRY'},
+				'PER': {fillKey: 'PER'},
+				'SUR': {fillKey: 'SUR'},
+				'URY': {fillKey: 'URY'},
+				'VEN': {fillKey: 'VEN'},
+				
 				
 			}
         
