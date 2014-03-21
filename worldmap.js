c9d4abd39697b881c9642bc3bd11d63810162392
whughes8 <wesjhughes@gatech.edu>
2014-03-21 10:42:36 -0400

    Transitions for countries included
    with Tim's help

--- worldmap.js
+++ worldmap.js
@@ -2,23 +2,15 @@ d3.csv("testCSV.csv", function(error, data){
 
 createMap(data);
 
-});
-var projectionMain = d3.geo.equirectangular()
-	.center([0, -100])
-	.rotate([0, 0])
-	.scale(150)
-	.translate([document.getElementById('container').offsetWidth / 2, document.getElementById('container').offsetWidth / 2]);
-var pathMain = d3.geo.path()
-	.projection(projectionMain);
-
+}); 
 var bombMap;
 var europeColor = '#9467bd';
-var africaColor = '#ffa500';
-var southAmericaColor = '#008000';
-var northAmericaColor = '#800080';
+var africaColor = '';
+var southAmericaColor = '';
+var northAmericaColor = '';
 var asiaColor = '#ff0000';
-var oceaniaColor = '#0000ff';
-var centralAmericaColor = '#ffff00';
+var austrailaColor = '';
+var p0 = [500,300,600]
 
 function createMap(data){
 	data.forEach(function(d){
@@ -27,30 +19,42 @@ function createMap(data){
 	redrawMap(0,0,0,0,150);
 	bubbles();
 	console.log(bombMap);
-	
-	bombMap.svg.selectAll('.datamaps-bubble')
-		.on('click', function() {
-			//redrawMap(0,0,0,0,400);
-				
-				projectionMain.transition()
-				.center([this.cx, this.cy])
-				.rotate([0, 0])
-				.scale(400)
-				//need to find a way to update the scale correctly
-				.translate([document.getElementById('container').offsetWidth / 2,  document.getElementById('container').offsetHeight / 2]);
-				pathMain.transition()
-				.projection(projectionMain)
-				.duration(100);
-			
-			}
-			);
-			
+	bombMap.svg.selectAll('.datamaps-bubble').on('click', function() {
+		//redrawMap(0,0,0,0,400);
+		p1 = [100,100,250];
+		bombMap.svg.select('.datamaps-subunits').call(transition, p0, p1);
+		p0 = p1;
 		
-	};	
-		
-	
+		bombMap.svg.select('.datamaps-bubble').selectAll("circle")
+		.transition()
+		.attr("opacity",0)
+		.delay(0)
+		.duraction(1000)
+
+	});
 
 	
+	
+	
+};	
+
+function transition(svg, start, end) {
+  var center = [1000 / 2, 600 / 2],
+      i = d3.interpolateZoom(start, end);
+
+  svg
+      .attr("transform", transform(start))
+    .transition()
+      .delay(250)
+      .duration(i.duration * 2)
+      .attrTween("transform", function() { return function(t) { return transform(i(t)); }; })
+      //.each("end", function() { d3.select(this).call(transition, end, start); });
+
+  function transform(p) {
+    var k = 600 / p[2];
+    return "translate(" + (center[0] - p[0] * k) + "," + (center[1] - p[1] * k) + ")scale(" + k + ")";
+  }
+};
 
 	
 	function redrawMap(center1,center2,rotate1,rotate2,scale){
@@ -65,7 +69,14 @@ function createMap(data){
 			},
 			
 			setProjection: function(element) {
-				return {path: pathMain, projection: projectionMain}; 
+				var projection = d3.geo.equirectangular()
+				.center([center1, center2])
+				.rotate([rotate1, rotate2])
+				.scale(scale)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+				var path = d3.geo.path()
+				.projection(projection);
+				return {path: path, projection: projection}; 
 			},
 		
 			fills: {
@@ -105,39 +116,10 @@ function createMap(data){
 				'TWN': asiaColor,
 				'TUR': asiaColor,
 				'VNM': asiaColor,
-				'EGY': africaColor,
-				'ETH': africaColor,
-				'LBR': africaColor,
-				'MAR': africaColor,
-				'ZAF': africaColor,
-				'AUS': oceaniaColor,
-				'NZL': oceaniaColor,
-				'CAN': northAmericaColor,
-				'USA': northAmericaColor,
+				
+				'CDN': northAmericaColor,
 				'MEX': northAmericaColor,
-				'CUB': northAmericaColor,
-				'DOM': northAmericaColor,
-				'HTI': northAmericaColor,
-				'JAM': northAmericaColor,
-				'BLZ': centralAmericaColor,
-				'CRI': centralAmericaColor,
-				'SLV': centralAmericaColor,
-				'GCA': centralAmericaColor,
-				'HND': centralAmericaColor,
-				'NIC': centralAmericaColor,
-				'PAN': centralAmericaColor,
-				'ARG': southAmericaColor,
-				'BOL': southAmericaColor,
-				'BRA': southAmericaColor,
-				'CHL': southAmericaColor,
-				'COL': southAmericaColor,
-				'ECU': southAmericaColor,
-				'GUY': southAmericaColor,
-				'PRY': southAmericaColor,
-				'PER': southAmericaColor,
-				'SUR': southAmericaColor,
-				'URY': southAmericaColor,
-				'VEN': southAmericaColor,
+				'CUBA': northAmericaColor,
 				
 				
 				defaultFill: '#000000'
@@ -179,40 +161,6 @@ function createMap(data){
 				'TWN': {fillKey: 'TWN'},
 				'TUR': {fillKey: 'TUR'},
 				'VNM': {fillKey: 'VNM'},
-				'EGY': {fillKey: 'EGY'},
-				'ETH': {fillKey: 'ETH'},
-				'LBR': {fillKey: 'LBR'},
-				'MAR': {fillKey: 'MAR'},
-				'ZAF': {fillKey: 'ZAF'},
-				'AUS': {fillKey: 'AUS'},
-				'NZL': {fillKey: 'NZL'},
-				'USA': {fillKey: 'USA'},
-				'CAN': {fillKey: 'CAN'},
-				'MEX': {fillKey: 'MEX'},
-				'CUB': {fillKey: 'CUB'},
-				'DOM': {fillKey: 'DOM'},
-				'HTI': {fillKey: 'HTI'},
-				'JAM': {fillKey: 'JAM'},
-				'BLZ': {fillKey: 'BLZ'},
-				'CRI': {fillKey: 'CRI'},
-				'SLV': {fillKey: 'SLV'},
-				'GCA': {fillKey: 'GCA'},
-				'HND': {fillKey: 'HND'},
-				'NIC': {fillKey: 'NIC'},
-				'PAN': {fillKey: 'PAN'},
-				'ARG': {fillKey: 'ARG'},
-				'BOL': {fillKey: 'BOL'},
-				'BRA': {fillKey: 'BRA'},
-				'CHL': {fillKey: 'CHL'},
-				'COL': {fillKey: 'COL'},
-				'ECU': {fillKey: 'ECU'},
-				'GUY': {fillKey: 'GUY'},
-				'PRY': {fillKey: 'PRY'},
-				'PER': {fillKey: 'PER'},
-				'SUR': {fillKey: 'SUR'},
-				'URY': {fillKey: 'URY'},
-				'VEN': {fillKey: 'VEN'},
-				
 				
 			}
         
